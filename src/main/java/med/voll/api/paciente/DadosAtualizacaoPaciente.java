53b5d3a4377ac44cd94833a9e2d2e6be9afe11f7
Saul C Batista <saulcbatista@gmail.com>
2023-09-07 14:15:56 -0300

    Atualizado o retorno do CRUD do paciente e adicionado o DTO de DadosDetalhamentoPaciente.

--- src/main/java/med/voll/api/paciente/DadosAtualizacaoPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosAtualizacaoPaciente.java
@@ -1,7 +1,6 @@
 package med.voll.api.paciente;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.endereco.DadosEndereco;
 
